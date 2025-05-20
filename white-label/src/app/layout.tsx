e36dccba8533de886d510c69e040f91a50ef02a7
brewinski <cbrewin93@gmail.com>
2025-05-20 13:41:49 +1000

    upd

--- white-label/src/app/layout.tsx
+++ white-label/src/app/layout.tsx
@@ -1,11 +1,9 @@
 
 
 import { Metadata } from 'next';
-import { getThemeTypeFromHeaders } from '@cns/utils/serverTheme';
-import ThemeLayout from '@cns/components/ThemeLayout';
 import './globals.css';
 import { ClerkProvider } from '@clerk/nextjs';
-import Head from 'next/head';
+import { cookies } from 'next/headers';
 
 export const metadata: Metadata = {
   title: 'Multi-domain App',
@@ -17,20 +15,24 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const cook = await cookies()
+  const clerkRedirectValue = cook.get("__clerk_redirect_count")
 
   return (
     <html lang="en">
       <meta name="robots" content="all" />
       <meta name="google-site-verification" content="P9z0uYnscP75CoOnAOCAU-7ekJUfzdkuDfEHKkB8yDk" />
       <head>
-        <script async>
-          const [key, value] = ['__clerk_synced', 'true'];
-          const url = new URL(window.location.href);
-          url.searchParams.set(key, value);
-          window.history.replaceState(window.history.state, '', url);
-          console.log("Running ClerkSyncUrlEffectComponent");
-        </script>
-
+        {clerkRedirectValue?.value === "3" && (
+          <script async>
+            if (window.location)
+            const [key, value] = ['__clerk_synced', 'true'];
+            const url = new URL(window.location.href);
+            url.searchParams.set(key, value);
+            window.history.replaceState(window.history.state, '', url);
+            console.log("Running ClerkSyncUrlEffectComponent");
+          </script>
+        )}
       </head>
       <body>
         <ClerkProvider
